85e4125eedd7736b850c27a295191fc2ffc10780
Kaushik Hrushikesh Avuti <134273945+kaushik-kaizoku@users.noreply.github.com>
2023-10-09 11:48:03 +0000

    container config

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 57483
+let accountEmailId = "kaushik@gmail.com"
+var accountPassword = "12345"
+accountCity = "hyderabad"
+let accountState 
+ 
+// accountId = 2 // not allowed
+accountEmailId = "hh@gmail.com"
+accountPassword = "34567"
+accountCity = "vizag"
+
+console.log(accountId)
+console.table([ accountEmailId, accountPassword, accountCity, accountState])
+
+// use let rather than var to avoid scope errors
