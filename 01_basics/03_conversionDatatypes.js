41e84eb36287662afbc159c70cc39173a6e9110f
Kaushik Hrushikesh Avuti <134273945+kaushik-kaizoku@users.noreply.github.com>
2023-10-10 08:30:51 +0000

    datatype conversion

---
+++ 01_basics/03_conversionDatatypes.js
@@ -0,0 +1,13 @@
+let numberstr = "33"
+
+let number = Number(numberstr)
+// console.log(number)
+// console.log(typeof number)
+// console.log(numberstr)
+// console.log(typeof numberstr)
+
+let bool = true
+// console.log(Number(bool))
+// console.log(Boolean(numberstr))
+
+
